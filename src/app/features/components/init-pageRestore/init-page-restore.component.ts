51fdc87eb2d1e73dc8be6c7e969da09dbce584ef
Xiomara Andrea Pulido <web@bc.local>
2024-11-08 15:01:39 +0100

    Integration

--- src/app/features/components/init-pageRestore/init-page-restore.component.ts
+++ src/app/features/components/init-pageRestore/init-page-restore.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { RestorePasswordComponent } from '../restore-password/restore-password.component';
 
 @Component({
-  selector: 'app-init-page',
+  selector: 'app-init-restore-page',
   standalone: true,
   imports: [RestorePasswordComponent],
   templateUrl: './init-page-restore.component.html',
